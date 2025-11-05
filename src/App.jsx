d617925a5dd8dc77d3bf721b6233df7c0cbcc5e2
zarfa <zarfamasood924@gmail.com>
2025-11-05 12:21:50 +0400

    feat: initial commit

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import Signup from "./features/auth/Signup";
 import ForgotPassword from "./features/auth/ForgetPassword";
 import ConfirmEmail from "./features/auth/confirmEmail";
 import ResetPassword from "./features/auth/resetPassword";
+import Subscribe from "./features/auth/Subscribe";
 
 function App() {
   return (
@@ -14,6 +15,8 @@ function App() {
         <Route path="/forgotpassword" element={<ForgotPassword />} />
         <Route path="/confirm" element={<ConfirmEmail />} />
         <Route path="/resetpassword" element={<ResetPassword />} />
+        <Route path="/subscribe" element={<Subscribe />} />
+
       </Routes>
     </Router>
   );
